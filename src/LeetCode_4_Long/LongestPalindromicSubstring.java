ba67fef9f02549d66ee93094e5fd693e2133644d
Scott <Scott.wu@limenamics.com>
2018-03-19 20:39:28 +0800

    LeetCode5

---
+++ src/LeetCode_4_Long/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+package LeetCode_4_Long;
+
+/*
+ * @Author: Scott_wu
+ * @Description:
+ * @Date: Created in 20:09 2018/3/19
+ */
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("bb"));
+    }
+}
---
+++ src/LeetCode_4_Long/Solution.java
@@ -0,0 +1,38 @@
+package LeetCode_4_Long;
+
+/*
+ * @Author: Scott_wu
+ * @Description:
+ * @Date: Created in 20:10 2018/3/19
+ */
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        int max = 1, j;
+        String string, result = s.substring(0,1);
+        for (int i = 0; i < length; i++){
+            j = length-1;
+            while (i < j){
+                while (s.charAt(j) != s.charAt(i)) j--;
+                string = s.substring(i,j+1);
+                if (palindromic(string) && max < string.length()){
+                    max  = string.length();
+                    result = string;
+                }
+                j--;
+            }
+        }
+        return result;
+    }
+
+    private boolean palindromic(String string) {
+        int length = string.length();
+        int j;
+        for (int i = 0; i < length; i++){
+            j = length - i - 1;
+            if (string.charAt(i) != string.charAt(j)) return false;
+        }
+        return true;
+    }
+}
